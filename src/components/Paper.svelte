63156efed7382ea4ab329b10b8a0e9b9ee64b981
Daniel <daniel@pothos.dev>
2022-10-16 20:09:36 +0200

    erasing

--- src/components/Paper.svelte
+++ src/components/Paper.svelte
@@ -11,22 +11,34 @@
   let recordedPoints: Point[][] = []
 
   function onPointerDown(event: PointerEvent) {
-    if (event.buttons == 1) {
-      mode = 'drawing'
+    if (event.buttons == 1) mode = 'drawing'
+    if (event.buttons == 2) mode = 'erasing'
+    if (mode == 'drawing') {
+      currentPoints = [[event.offsetX, event.offsetY]]
     }
-    if (event.buttons == 2) {
-      mode = 'erasing'
-    }
-    currentPoints = [[event.offsetX, event.offsetY]]
   }
   function onPointerMove(event: PointerEvent) {
     if (mode == 'drawing') {
       currentPoints = [...currentPoints, [event.offsetX, event.offsetY]]
     }
+    if (mode == 'erasing') {
+      // Drop all polylines that are close to the pointer (10px)
+      const minDist = 10
+      recordedPoints = recordedPoints.filter(points =>
+        points.every(point => {
+          const dx = point[0] - event.offsetX
+          const dy = point[1] - event.offsetY
+          const sqDist = dx * dx + dy * dy
+          return sqDist > minDist * minDist
+        })
+      )
+    }
   }
   function onPointerUp() {
     mode = 'idle'
-    recordedPoints = [...recordedPoints, currentPoints]
+    if (currentPoints.length > 1) {
+      recordedPoints = [...recordedPoints, currentPoints]
+    }
     currentPoints = []
   }
 </script>
