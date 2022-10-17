0686d5fbd15d9568682222e0257114bc6f47a0bb
Daniel Ebert <pancomputationalist@mailbox.org>
2022-10-17 14:37:55 +0000

    enable hmr on codespaces

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	server: {
+		hmr: {
+			clientPort: 443,
+		},
+	}
 };
 
 export default config;
